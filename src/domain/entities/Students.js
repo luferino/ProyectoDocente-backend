79c75809ab3983bea2f4d1ab5b4a3940c4baaace
Luis Benegas <luisbene12@gamil.com>
2025-12-31 22:06:52 -0300

    agregando cosas al domain

---
+++ src/domain/entities/Students.js
@@ -0,0 +1,12 @@
+export class student {
+    constructor ({id, nombre, apellido, documento, tipo_documento, celular, email}) {
+        if (!nombre || !email) throw new Error('Name and email are required');
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = documento;
+        this.tipo_documento = tipo_documento;
+        this.celular = celular;
+        this.email = email;
+    }
+}
---
+++ src/domain/repositories/StudenRepository.js
@@ -0,0 +1,8 @@
+export class StudentRepository {
+    create (student) {
+        throw new Error('Method not implemented');
+    }
+    findAll () {
+        throw new Error('Method not implemented');
+    }
+}
