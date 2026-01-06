4d0618536ad27a33a8564425a462328901e277fe
Luis Benegas <luisbene12@gmail.com>
2026-01-05 21:02:27 -0300

    cargando materias

---
+++ src/domain/entities/Grades.js
@@ -0,0 +1,8 @@
+export class Grade {
+    constructor ({id, studentId, subjectId, value}){
+        this.id = id;
+        this.studentId = studentId;
+        this.subjectId = subjectId;
+        this.value = value;
+    }
+}
---
+++ src/domain/repositories/GradeRepository.js
@@ -0,0 +1,5 @@
+export class GradeRepository {
+    assign(grade, client) {
+        throw new Error('Method not implemented');
+    }
+}
