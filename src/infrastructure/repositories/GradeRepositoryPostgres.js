bd97965f385abf90648124b7583a312bc2f986cb
Luis Benegas <luisbene12@gmail.com>
2026-01-07 20:36:50 -0300

    se crea el reposytory

---
+++ src/infrastructure/repositories/GradeRepositoryPostgres.js
@@ -0,0 +1,19 @@
+import { GradeRepository } from '../../domain/repositories/GradeRepository.js';
+import { Grade } from '../../domain/entities/Grade.js';
+
+export class GradeRepositoryPostgres extends GradeRepository {
+    async assign({ studentId, subjectId, value }, client) {
+        const result = await client.query(
+            'INSERT INTO notas (student_id, materia_id, value) VALUES ($1, $2, $3) RETURNING id, student_id, materia_id, value',
+            [studentId, subjectId, value]
+        );
+
+        const row = result.rows[0];
+        return new Grade({
+            id: row.id,
+            studentId: row.student_id,
+            subjectId: row.materia_id,
+            value: row.value
+        });
+    }
+}
