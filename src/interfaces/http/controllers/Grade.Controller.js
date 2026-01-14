3abc66b4a4539c8cededde71d73559028aaeacea
Luis Benegas <luisbene12@gmail.com>
2026-01-14 20:54:38 -0300

    agregar al controller

--- src/interfaces/http/controllers/Grade.Controller.js
+++ src/interfaces/http/controllers/Grade.Controller.js
@@ -2,6 +2,7 @@ export class GradeController {
     constructor(assignGradeUseCase) {
         this.assignGradeUseCase = assignGradeUseCase;
         this.assign = this.assign.bind(this);
+        this.getByStudent = this.getByStudent.bind(this);
     }
 
     async assign(req, res) {
@@ -9,6 +10,19 @@ export class GradeController {
             const grade = await this.assignGradeUseCase.execute(req.body);
             res.status(201).json(grade);
         } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+
+    async getByStudent(req, res) {
+        try {
+            const { studentId } = req.params;
+            const result = await this.getGradeByStudentUseCase.execute(
+                Number(studentId)
+            );
+            res.status(200).json(result);
+        }
+        catch (error) {
             res.status(400).json({ error: error.message });
         }
     }
