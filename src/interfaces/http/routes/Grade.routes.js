6bf7b058d3e01ddb9ab281a93bf2d5a37f80b944
Luis Benegas <luisbene12@gmail.com>
2026-01-15 20:32:38 -0300

    modificando las rutas

--- src/interfaces/http/routes/Grade.routes.js
+++ src/interfaces/http/routes/Grade.routes.js
@@ -3,15 +3,18 @@ import { pool } from "../../../infrastructure/database/postgres/connection.js";
 import { GradeRepositoryPostgres } from "../../../infrastructure/repositories/GradeRepositoryPostgres.js";
 import { AssignGradeUseCase } from "../../../aplication/use-case/grades/assing-grade.usecase.js";
 import { GradeController } from "../controllers/Grade.Controller.js";
+import { GetGradeByStudentUseCase } from '../../../aplication/use-case/grades/get-grade-by-student.usecase.js';
 
 export const gradeRoutes = () => {
     const router = Router();
 
     const repository = new GradeRepositoryPostgres();
-    const useCase = new AssignGradeUseCase(pool, repository);
-    const controller = new GradeController(useCase);
+    const assingUseCase = new AssignGradeUseCase(pool, repository);
+    const getGradeByStudentUseCase = new GetGradeByStudentUseCase(repository);
+    const controller = new GradeController(assingUseCase, getGradeByStudentUseCase);
 
     router.post("/", controller.assign);
+    router.get("/student/:studentId", controller.getByStudent);
 
     return router;
 }
