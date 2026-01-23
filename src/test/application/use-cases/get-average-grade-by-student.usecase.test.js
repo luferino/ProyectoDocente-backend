55aa2238c3ca9ac9c5154ef0ac96a20018b4ff74
Luis Benegas <luisbene12@gmail.com>
2026-01-23 20:53:58 -0300

    Se crea un archivo de test

---
+++ src/test/application/use-cases/get-average-grade-by-student.usecase.test.js
@@ -0,0 +1,2 @@
+import {GetGradeByStudentUseCase} from '../../../application/use-case/grades/get-grade-by-student.usecase.js';
+import { FacekGradeRepository } from '../../fakes/FackeGradeRepository.js';
