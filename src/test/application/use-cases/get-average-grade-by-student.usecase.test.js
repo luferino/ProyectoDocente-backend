c0436662b2baa432f7536850adbb4bded8325904
Luis Benegas <luisbene12@gmail.com>
2026-01-24 21:19:13 -0300

    se completa el test

--- src/test/application/use-cases/get-average-grade-by-student.usecase.test.js
+++ src/test/application/use-cases/get-average-grade-by-student.usecase.test.js
@@ -1,3 +1,28 @@
 import {GetGradeByStudentUseCase} from '../../../application/use-case/grades/get-grade-by-student.usecase.js';
 import { FacekGradeRepository } from '../../fakes/FackeGradeRepository.js';
 
+export class GetAverageGradeByStudentUseCase {
+    constructor(gradeRepository){
+        this.gradeRepository = gradeRepository;
+    }
+
+    async execute(studentId){
+        const row = (await this.gradeRepository.getAverageGradeByStudent(studentId)) || [];
+
+        if (row.length === 0) {
+            throw new Error('No grades found for the student');
+        }
+
+        const total = row.reduce((sum, r) => sum + r.value, 0);
+        const average = total / row.length;
+
+        return {
+            student: {
+                id: rows[0].studentId,
+                name: rows[0].studen_name,
+            },
+            average
+        };
+    }
+}
+
