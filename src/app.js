48db87b4434cd5340c50c7c09d5c48e22b52daa3
Luis Benegas <luisbene12@gmail.com>
2026-01-27 19:06:04 -0300

    se modifica app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import { studentRoutes } from './interfaces/http/routes/Student.routes.js';
 import { gradeRoutes } from './interfaces/http/routes/Grade.routes.js';
-
+import { buildGradeModule } from './infrastructure/grades/grade.module.js';
+import { createGradeRoutes } from './infrastructure/grades/grade.routes.js';
 
 export function createApp() {
-  const app = express(); 
+  const app = express();
+  const gradeController = buildGradeModule();
 
   app.use(express.json());
 
   app.use('/students', studentRoutes());
   app.use('/grades', gradeRoutes());
+  app.use('/api', createGradeRoutes(gradeController));
 
   return app;
 }
