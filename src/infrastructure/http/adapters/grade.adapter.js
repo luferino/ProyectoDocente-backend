b19425e273f5bd33e70b3d62c3aa6a1b19e78a18
Luis Benegas <luisbene12@gmail.com>
2026-01-31 11:02:43 -0300

    se cambian nombres de repository

--- src/infrastructure/http/adapters/grade.adapter.js
+++ src/infrastructure/http/adapters/grade.adapter.js
@@ -1,1 +1,13 @@
-46bc73d6191c98ea4280dcd9ff6bae293f8fb8c0
+// infrastructure/http/adapters/grade.adapter.js
+export class GradeAdapter {
+    static toDomain(row) {
+        return {
+            student: {
+                id: row.student_id,
+                name: row.student_name
+            },
+            subject: row.subject_name,
+            value: row.value
+        };
+    }
+}
