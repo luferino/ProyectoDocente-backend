2153a78f77cb16d5c58a8e09761efb6f4cd0159c
Luis Benegas <luisbene12@gmail.com>
2026-02-22 18:42:43 -0300

    seajusta entity

--- src/domain/entities/grade.entity.js
+++ src/domain/entities/grade.entity.js
@@ -1,4 +1,6 @@
 import { GradeValue } from '../value-objects/grade-value.vo.js';
+import { Result } from '../shared/result.js';
+
 export class Grade {
     constructor ({id, studentId, studentName,subjectId,subjectName, value}){
         this.id = id;
@@ -8,6 +10,21 @@ export class Grade {
         this.subjectName = subjectName;
         this.value = new GradeValue(value);
 
+    }
+    static create(data) {
+        const gradeOrError = GradeValue.create(data.value);
+
+        if (gradeOrError.isFailure()) {
+            return Result.fail(gradeOrError.error);
+        }
+        return Result.ok(new Grade({
+            id: data.id,
+            studentId: data.studentId,
+            studentName: data.studentName,
+            subjectId: data.subjectId,
+            subjectName: data.subjectName, 
+            value: gradeOrError.value
+        }));
     }
 
     isApproved() {
