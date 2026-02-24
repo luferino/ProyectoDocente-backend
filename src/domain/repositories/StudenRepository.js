de3af815f8be22ed5461db318b8e4272dabaf873
Luis Benegas <luisbene12@gmail.com>
2026-02-24 20:28:50 -0300

    se modifica repository

--- src/domain/repositories/StudenRepository.js
+++ src/domain/repositories/StudenRepository.js
@@ -1,8 +1,16 @@
 export class StudentRepository {
+    async findById(studentId) {
+        throw new Error('Method not implemented');
+    }
+
+    async save(student){
+        throw new Error('Method not implemented');
+    }
+    /*
     create (student) {
         throw new Error('Method not implemented');
     }
     findAll () {
         throw new Error('Method not implemented');
-    }
+    }*/
 }
