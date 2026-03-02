41b96035bfc5e88fae3d0adc8496a4f6d8dc3810
Luis Benegas <luisbene12@gmail.com>
2026-03-02 17:58:52 -0300

    se modifica controller

--- src/infrastructure/config/dependencies.js
+++ src/infrastructure/config/dependencies.js
@@ -1,12 +1,15 @@
 import {GetGradesByStudentHandler} from '../../application/queries/get-grades-by-student/get-grades-by-student.handler.js';
 import {GradeRepository} from '../../domain/repositories/GradeRepository.js';
+import {GetAverageGradeByStudentHandler} from '../../application/queries/get-grades-by-student/get-average-grade-by-student.handler.js';
 
 const gradeRepository = new GradeRepository();
 
 const getGradeByStudentHandle = new GetGradesByStudentHandler(gradeRepository);
+const getAverageGradeByStudentHandle = new GetAverageGradeByStudentHandler(gradeRepository);
 
 export const dependencies = {
     gradeController: new GradeController({
-        getGradeByStudentHandle
+        getGradeByStudentHandle,
+        getAverageGradeByStudentHandle
     })
 }
