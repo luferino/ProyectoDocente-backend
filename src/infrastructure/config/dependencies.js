0c3f2251179ecfd57c23b3e3d07a1501e0ec7a79
Luis Benegas <luisbene12@gmail.com>
2026-03-05 21:11:06 -0300

    dependenci

--- src/infrastructure/config/dependencies.js
+++ src/infrastructure/config/dependencies.js
@@ -1,15 +1,18 @@
 import {GetGradesByStudentHandler} from '../../application/queries/get-grades-by-student/get-grades-by-student.handler.js';
 import {GradeRepository} from '../../domain/repositories/GradeRepository.js';
 import {GetAverageGradeByStudentHandler} from '../../application/queries/get-grades-by-student/get-average-grade-by-student.handler.js';
+import {AddGradeHandler} from '../../application/commands/add-grade/add-grade.handler.js';
 
 const gradeRepository = new GradeRepository();
 
 const getGradeByStudentHandle = new GetGradesByStudentHandler(gradeRepository);
 const getAverageGradeByStudentHandle = new GetAverageGradeByStudentHandler(gradeRepository);
-
+const addGradeHandler = new AddGradeHandler(gradeRepository);
+    
 export const dependencies = {
     gradeController: new GradeController({
         getGradeByStudentHandle,
-        getAverageGradeByStudentHandle
+        getAverageGradeByStudentHandle,
+        addGradeHandler
     })
 }
